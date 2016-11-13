c380ebf4b8c172f7ec7c89a47d587a0b0fb2a982
Loriah Pope <loriahpope@Loriahs-MacBook-Pro.local>
2016-11-13 00:30:22 -0500

    initial commit

---
+++ files/home/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,i,j,[k],l,_(m,n,o,p,q,r,s,_(),t,_(u,v,w,x,y,_(z,A,B,C),D,null,E,x,F,x,G,H,I,null,J,K,L,M,N,O,P,K),Q,_(),R,_(),S,_(T,[])),U,_(),V,_());}; 
+var b="url",c="home.html",d="generationDate",e=new Date(1479014813986.48),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i=true,j="variables",k="OnLoadVariable",l="page",m="packageId",n="a78f2ad2e6094c94b18ac94ea041cc3b",o="type",p="Axure:Page",q="name",r="Home",s="notes",t="style",u="baseStyle",v="627587b6038d43cca051c114ac41ad32",w="pageAlignment",x="near",y="fill",z="fillType",A="solid",B="color",C=0xFFFFFFFF,D="image",E="imageHorizontalAlignment",F="imageVerticalAlignment",G="imageRepeat",H="auto",I="favicon",J="sketchFactor",K="0",L="colorStyle",M="appliedColor",N="fontName",O="Applied Font",P="borderWidth",Q="adaptiveStyles",R="interactionMap",S="diagram",T="objects",U="masters",V="objectPaths";
+return _creator();
+})());
